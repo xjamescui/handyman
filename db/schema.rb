7c60f780cbd5d268a3e32df9df5acded26c3f0ec
James Cui <james.q.cui@gmail.com>
2014-07-07 10:25:03 -0400

    Added product description

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140705205548) do
+ActiveRecord::Schema.define(version: 20140707141623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20140705205548) do
     t.datetime "updated_at"
     t.string   "image_name"
     t.integer  "rent_item_id"
+    t.string   "description"
   end
 
 end
