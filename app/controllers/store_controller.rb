40d494858aa515f49fe6b8159906df78a1a10e1e
James Cui <james.q.cui@gmail.com>
2014-07-08 13:05:27 -0400

    add Rental tracking

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,5 +3,6 @@ class StoreController < ApplicationController
     @rentables = Rentable.all
     @warehouses = ["Warehouse A", "Warehouse B", "Warehouse C", "Warehouse D"]
     @stores = ["Hamilton", "Waterloo", "London", "Barrie"]
+    @customers = ["Customer", "John Doe", "Nancy Smith", "Gandalf Gray", "Bilbo Baggins", "Frodo", "Orlando Bloom"]
   end
 end
